bc54f5fbc2339dd81ec79ef04b7576b69b5f1d89
ShelaevNikita <shelaev.nr@edu.spbstu.ru>
2018-09-30 22:38:54 +0300

    Версия "последняя надежда".5

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -107,9 +107,8 @@ fun thirdDigit(number: Int): Int = number % 1000 / 100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-val travelMinutes =
-        fun(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
-                (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
 
 
 /**
@@ -119,9 +118,8 @@ val travelMinutes =
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-val accountInThreeYears =
-        fun(initial: Int, percent: Int): Double =
-                initial * pow(1 + percent * 0.01, 3.0)
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+        initial * pow(1 + percent * 0.01, 3.0)
 
 /**
  * Простая
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-val isNumberHappy = fun(number: Int): Boolean =
+fun isNumberHappy(number: Int): Boolean =
         number % 10 + number % 100 / 10 == number % 1000 / 100 + number / 1000
 
 /**
